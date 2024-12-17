82ddff196b70addba92fb681586ff4d6132a440d
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-18 01:52:10 +0200

    Updating Input Widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/widgets/input.dart';
 
 class HomePage extends StatelessWidget {
 
--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -3,20 +3,23 @@ import 'package:flutter/material.dart';
 class Input extends StatelessWidget {
    final String text;
    final Icon icon;
+   final double height;
+   final int maxLines;
 
-   const Input({super.key, required this.text, required this.icon});
+   const Input({super.key, required this.text, required this.icon, required this.height, required this.maxLines});
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 320,
-      height: 60,
+      width: 270,
+      height: height,
       padding: EdgeInsets.fromLTRB(20,0,20,0),
       
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
+        borderRadius: BorderRadius.circular(10),
         color:Theme.of(context).secondaryHeaderColor,
       ),
       child: TextFormField(
+          maxLines: maxLines,
           decoration: InputDecoration(
           filled: true,
           fillColor: Theme.of(context).secondaryHeaderColor,
