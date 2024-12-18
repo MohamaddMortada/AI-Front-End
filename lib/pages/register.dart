2bfda5149019af3584621e8270aff7d05ad56c51
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-18 20:37:43 +0200

    Creating Register Page

---
+++ lib/pages/register.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:front_end/widgets/button.dart';
+import 'package:front_end/widgets/input.dart';
+import 'package:front_end/widgets/main_logo.dart';
+import 'package:front_end/widgets/text_field.dart';
+
+class Register extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+          
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MainLogo(),
+              Text_Field(
+                text: 'ATHLETIC PERFORMANCE',
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+              Spacer(),
+              Input(text: 'Username', icon: Icon(Icons.person), height: 45, maxLines: 1),
+              SizedBox(height: 10),
+              Input(text: 'Email', icon: Icon(Icons.email), height: 45, maxLines: 1),
+              SizedBox(height: 10),
+              Input(text: 'Phone Number', icon: Icon(Icons.phone), height: 45, maxLines: 1),
+              SizedBox(height: 10),
+              Input(text: 'Password', icon: Icon(Icons.key), height: 45, maxLines: 1),
+              SizedBox(height: 10),
+              Input(text: 'Confirm Password', icon: Icon(Icons.key), height: 45, maxLines: 1),
+              SizedBox(height: 20),
+              Main_Button(text: 'Register',icon: Icon(Icons.app_registration_rounded),),
+              SizedBox(height: 10),
+              Text('OR'),
+              SizedBox(height: 10),
+              Main_Button(text: 'Register with Google',icon: Icon(Icons.app_registration_rounded),),
+              Spacer(),
+            ],
+          )
+        )
+    );
+  }
+}
