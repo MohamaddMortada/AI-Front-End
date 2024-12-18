f41b33fd512eb21dabbcb62a836bda80b5381fa5
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-18 12:07:27 +0200

    Creating updateBallPosition Method

--- lib/controllers/assistive_ball_controller.dart
+++ lib/controllers/assistive_ball_controller.dart
@@ -2,10 +2,18 @@ import 'package:get/get.dart';
 
 class AssistiveBallController extends GetxController {
   var isExpanded = false.obs; 
- 
+  var ballX = 0.0.obs; 
+  var ballY = 0.0.obs; 
 
   void toggleExpansion() {
     isExpanded.value = !isExpanded.value;
+  }
+
+  void updateBallPosition(double dx, double dy) {
+    if (!isExpanded.value) {
+      ballX.value += dx;
+      ballY.value += dy;
+    }
   }
 
   void collapseBall() {
