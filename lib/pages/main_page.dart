8a4f6c776d78a5c0388b379f2f46ae2d35d69655
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-18 23:57:27 +0200

    Editing MainPage

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,36 +1,47 @@
 
 import 'package:flutter/material.dart';
 import 'package:front_end/widgets/image_button.dart';
+import 'package:front_end/widgets/profile_widget.dart';
+import 'package:front_end/widgets/text_field.dart';
 
 class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-        body: Center(
+        body: Column(
           
-          child:Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              Align(
+                alignment: Alignment.topRight,
+                  child:Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+                    child: ProfileIcon(imageUrl: 'https://pics.craiyon.com/2023-10-20/ff5136d14c0f415faa9cd7e7b654a277.webp', rad: 24),
+
+              ),),
+              
+              Text_Field(text: 'Welcome Nabiha \n to AP', fontSize: 24, fontWeight: FontWeight.bold),
               Spacer(),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  ImageButton(imagePath: 'assets/Detect-Image.webp', text: 'DETECT'),
-                  ImageButton(imagePath: 'assets/Analyze-Image.webp', text: 'ANALYZE'),
+                  ImageButton(imagePath: 'assets/Detect-Image.webp', text: 'DETECT', discription: 'DETECT & FIX \nERRORS',),
+                  ImageButton(imagePath: 'assets/Analyze-Image.webp', text: 'ANALYZE', discription: 'ANALYZE YOUR PERFORMANCE',),
                 ],
               ),
               SizedBox(height: 50),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  ImageButton(imagePath: 'assets/Predict-Image.webp', text: 'PREDICT'),
-                  ImageButton(imagePath: 'assets/Calculate-Image.webp', text: 'CALCULATE'),
+                  ImageButton(imagePath: 'assets/Predict-Image.webp', text: 'PREDICT', discription: 'PREDICT FUTURE PERFORMANCES',),
+                  ImageButton(imagePath: 'assets/Calculate-Image.webp', text: 'CALCULATE', discription: 'CALCULATE YOUR \nPOINTS',),
                 ],
               ),
               Spacer(),
+              Spacer(),
             ]
           )
-        )
-    );
+        );
+    
   }
 }
--- lib/widgets/image_button.dart
+++ lib/widgets/image_button.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/widgets/text_field.dart';
 class ImageButton extends StatelessWidget {
   final String imagePath;
   final String text;
-  const ImageButton({super.key, required this.imagePath, required this.text});
+  final String discription;
+  const ImageButton({super.key, required this.imagePath, required this.text, required this.discription});
 
   @override
   Widget build(BuildContext context) {
@@ -11,18 +13,23 @@ class ImageButton extends StatelessWidget {
             onTap: () {
               print('Clicked');
             },
-            child:Container(
+            child:
+            Container(
       decoration: BoxDecoration(
       color: Theme.of(context).primaryColor,
       borderRadius: BorderRadius.circular(10)
     ),
 
     width:160,
-    height: 173,
+    height: 220,
     padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
       child: Column(
         mainAxisAlignment : MainAxisAlignment.spaceBetween,
         children: [
+          Container(
+            color: Theme.of(context).scaffoldBackgroundColor,
+            height: 40,
+            child: Text_Field(text: discription, fontSize: 12, fontWeight: FontWeight.w800),),
             Image.asset(
                 imagePath,
                 width:double.infinity,
