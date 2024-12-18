891e667d2b74ce174e14b2258d6247e832ea02e1
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-18 12:22:25 +0200

    Creating AssistiveBall Widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/widgets/assistive_ball.dart';
 import 'package:front_end/widgets/input.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    return  Scaffold(
+      body: Center(child: AssistiveBall()),
     );
   }
 }
--- lib/widgets/assistive_ball.dart
+++ lib/widgets/assistive_ball.dart
@@ -1,8 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/controllers/assistive_ball_controller.dart';
+import 'package:get/get.dart';
 
 class AssistiveBall extends StatelessWidget {
 
+  final AssistiveBallController controller = Get.put(AssistiveBallController());
 
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          controller.collapseBall(); 
+        },
+        onPanUpdate: (details) {
+          controller.updateBallPosition(details.delta.dx, details.delta.dy); 
+        },
+        child: Stack(
+          children: [
+            Obx(() {
+              return Positioned(
+                left: MediaQuery.of(context).size.width / 2 + controller.ballX.value - 40,
+                top: MediaQuery.of(context).size.height / 2 + controller.ballY.value - 40,
+                child: GestureDetector(
+                  onTap: controller.toggleExpansion, 
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 300),
+                    width: 80,
+                    height: controller.isExpanded.value ? 300 : 80, 
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(40),
+                      
+                    ),
+                    child: controller.isExpanded.value
+                        ? BuildIconsColumn() 
+                        : const Icon(Icons.touch_app,
+                            color: Colors.white, size: 20), 
+                  ),
+                ),
+              );
+            }),
+          ],
+        ),
+      ),
+    );
+  }
   Widget IconItem(IconData icon, String tooltip) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
@@ -13,7 +55,7 @@ class AssistiveBall extends StatelessWidget {
     );
   }
   
-    Widget _buildIconsColumn() {
+    Widget BuildIconsColumn() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -24,9 +66,5 @@ class AssistiveBall extends StatelessWidget {
       ],
     );
   }
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+
 }
