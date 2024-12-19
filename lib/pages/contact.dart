2b1d8711f72b10d60afd1fc1398a934ebda8eece
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-19 11:46:05 +0200

    Fixing AssistiveBall Page

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/widgets/assistive_ball.dart';
 import 'package:front_end/widgets/button.dart';
 import 'package:front_end/widgets/input.dart';
 import 'package:front_end/widgets/profile_bar.dart';
-import 'package:front_end/widgets/text_field.dart';
 
 class Contact extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-        body: Center(
+    return   Scaffold(
+        body: Stack(
+          children: [
+            Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center, children: [
           ProfileBar(),
+          
           Spacer(),
           Input(text: 'Type your message here', icon: Icon(Icons.message), height: 270, maxLines: 10, ),
           Main_Button(text:'Send Message', icon: Icon(Icons.email)),
           Spacer(),
-        ])));}}
+          Spacer(),
+        ])),
+        AssistiveBall(),
+          ],
+        ),
+        );}}
--- lib/widgets/assistive_ball.dart
+++ lib/widgets/assistive_ball.dart
@@ -3,49 +3,51 @@ import 'package:front_end/controllers/assistive_ball_controller.dart';
 import 'package:get/get.dart';
 
 class AssistiveBall extends StatelessWidget {
-
   final AssistiveBallController controller = Get.put(AssistiveBallController());
 
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GestureDetector(
-        onTap: () {
-          controller.collapseBall(); 
-        },
-        onPanUpdate: (details) {
-          controller.updateBallPosition(details.delta.dx, details.delta.dy); 
-        },
-        child: Stack(
-          children: [
-            Obx(() {
-              return Positioned(
-                left: MediaQuery.of(context).size.width / 2 + controller.ballX.value - 40,
-                top: MediaQuery.of(context).size.height / 2 + controller.ballY.value - 40,
-                child: GestureDetector(
-                  onTap: controller.toggleExpansion, 
-                  child: AnimatedContainer(
-                    duration: const Duration(milliseconds: 300),
-                    width: 80,
-                    height: controller.isExpanded.value ? 300 : 80, 
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).primaryColor,
-                      borderRadius: BorderRadius.circular(40),
-                      
-                    ),
-                    child: controller.isExpanded.value
-                        ? BuildIconsColumn() 
-                        : const Icon(Icons.touch_app,
-                            color: Colors.white, size: 20), 
+    return GestureDetector(
+      onTap: () {
+        controller.collapseBall();
+      },
+      onPanUpdate: (details) {
+        controller.updateBallPosition(details.delta.dx, details.delta.dy);
+      },
+      child: Stack(
+        children: [
+          Obx(() {
+            return Positioned(
+              left: MediaQuery.of(context).size.width / 2 +
+                  controller.ballX.value -
+                  40,
+              top: MediaQuery.of(context).size.height / 2 +
+                  controller.ballY.value -
+                  40,
+              child: GestureDetector(
+                onTap: controller.toggleExpansion,
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  width: 80,
+                  height: controller.isExpanded.value ? 300 : 80,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(40),
                   ),
+                  child: controller.isExpanded.value
+                      ? _buildIconsColumn()
+                      : const Icon(Icons.touch_app,
+                          color: Colors.white, size: 20),
                 ),
-              );
-            }),
-          ],
-        ),
+              ),
+            );
+          }),
+        ],
       ),
     );
   }
-  Widget IconItem(IconData icon, String tooltip) {
+
+  Widget _iconItem(IconData icon, String tooltip) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: Tooltip(
@@ -54,17 +56,16 @@ class AssistiveBall extends StatelessWidget {
       ),
     );
   }
-  
-    Widget BuildIconsColumn() {
+
+  Widget _buildIconsColumn() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconItem(Icons.home, "Home"),
-        IconItem(Icons.chat, "AI Chatbot"),
-        IconItem(Icons.person, "Profile"),
-        IconItem(Icons.email_outlined, "Contact Us"),
+        _iconItem(Icons.home, "Home"),
+        _iconItem(Icons.chat, "AI Chatbot"),
+        _iconItem(Icons.person, "Profile"),
+        _iconItem(Icons.email_outlined, "Contact Us"),
       ],
     );
   }
-
 }
