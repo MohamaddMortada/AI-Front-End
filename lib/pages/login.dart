0ed6b683eba1ad3bff92b0811adfbef18f76bc9c
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-24 11:25:02 +0200

    Adjusting Controller inside Login Page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:front_end/controllers/login_controller.dart';
 import 'package:front_end/widgets/button.dart';
 import 'package:front_end/widgets/input.dart';
 import 'package:front_end/widgets/main_logo.dart';
 import 'package:front_end/widgets/text_field.dart';
 
 class Login extends StatelessWidget {
-  
+  final LoginController controller = LoginController();
+
   final TextEditingController nameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +28,7 @@ class Login extends StatelessWidget {
               Spacer(),
               Input(text: 'Username', icon: Icon(Icons.person), height: 45, maxLines: 1, controller: nameController,),
               SizedBox(height: 10),
-              Input(text: 'Password', icon: Icon(Icons.key), height: 45, maxLines: 1, controller: emailController,),
+              Input(text: 'Password', icon: Icon(Icons.key), height: 45, maxLines: 1, controller: passwordController,),
               SizedBox(height: 10),
               Text_Field(
                 text: 'Forgot your Password',
@@ -34,7 +36,15 @@ class Login extends StatelessWidget {
                 fontWeight: FontWeight.normal,
               ),
               SizedBox(height: 10),
-              Main_Button(text: 'Login',icon: Icon(Icons.login), route: '/main', onTap: () {  },),
+              Main_Button(
+                text: 'Login',icon: Icon(Icons.login), route: '/main', 
+                onTap: () { 
+                   controller.loginUser(
+                    context,
+                  nameController.text.trim(),
+                  passwordController.text.trim(),
+                   );
+                 },),
               SizedBox(height: 10),
               Spacer(),
 
