cb71780546ce2579302e43e390cfdc2b2490382c
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-24 16:29:57 +0200

    Adding middleware for routes

--- lib/controllers/routes.dart
+++ lib/controllers/routes.dart
@@ -1,3 +1,4 @@
+import 'package:front_end/controllers/auth_middleware.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 
 import 'package:front_end/pages/analyze.dart';
@@ -17,17 +18,19 @@ import 'package:front_end/pages/register.dart';
 
 List<GetPage> appRoutes = [
 
-  GetPage(name: '/main', page: () => MainPage()),  
-        GetPage(name: '/detect', page: () => Detect()), 
-        GetPage(name: '/analyze', page: () => Analyze()), 
-        GetPage(name: '/predict', page: () => Predict()),
-        GetPage(name: '/calculate', page: () => Calculate()),
         GetPage(name: '/register', page: () => Register()),
         GetPage(name: '/login', page: () => Login()),
-        GetPage(name: '/detecting', page: () => Detecting()),
-        GetPage(name: '/analyzing', page: () => Analyzing()),
-        GetPage(name: '/predicting', page: () => Predicting()),
-        GetPage(name: '/profile', page: () => Profile()),
-        GetPage(name: '/contact', page: () => Contact()),
-        GetPage(name: '/chatbot', page: () => Chatbot()),
+
+        GetPage(name: '/main', page: () => MainPage(),middlewares: [AuthMiddleware()],),  
+        GetPage(name: '/detect', page: () => Detect(),middlewares: [AuthMiddleware()],), 
+        GetPage(name: '/analyze', page: () => Analyze(),middlewares: [AuthMiddleware()],), 
+        GetPage(name: '/predict', page: () => Predict(),middlewares: [AuthMiddleware()],),
+        GetPage(name: '/calculate', page: () => Calculate(),middlewares: [AuthMiddleware()],),
+        
+        GetPage(name: '/detecting', page: () => Detecting(),middlewares: [AuthMiddleware()],),
+        GetPage(name: '/analyzing', page: () => Analyzing(),middlewares: [AuthMiddleware()],),
+        GetPage(name: '/predicting', page: () => Predicting(),middlewares: [AuthMiddleware()],),
+        GetPage(name: '/profile', page: () => Profile(),middlewares: [AuthMiddleware()],),
+        GetPage(name: '/contact', page: () => Contact(),middlewares: [AuthMiddleware()],),
+        GetPage(name: '/chatbot', page: () => Chatbot(),middlewares: [AuthMiddleware()],),
 ];
