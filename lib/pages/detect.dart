2b9d6f74d2d8ed38884ea4d0cf9d8aed8f1ddab5
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-12-31 00:09:23 +0200

    Adjusting Controller to Android

--- lib/pages/detect.dart
+++ lib/pages/detect.dart
@@ -2,78 +2,107 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:front_end/pages/detecting.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:front_end/widgets/profile_bar.dart';
 import 'package:front_end/widgets/assistive_ball.dart';
 import 'package:front_end/widgets/button.dart';
-import 'package:front_end/widgets/profile_bar.dart';
-import 'package:front_end/widgets/upload.dart';
-import 'package:image_picker/image_picker.dart';
-
 
 class Detect extends StatefulWidget {
-
   @override
   _DetectState createState() => _DetectState();
 }
+
 class _DetectState extends State<Detect> {
-  File? _image; 
+  File? _image;
   final picker = ImagePicker();
+  String _result = '';
+  String _errorMessage = '';
 
   Future<void> _uploadImage() async {
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
       setState(() {
         _image = File(pickedFile.path);
+        _errorMessage = '';
       });
     }
   }
 
   Future<void> _detectImage(BuildContext context) async {
     if (_image == null) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Please upload an image first.')),
-      );
+      setState(() {
+        _errorMessage = 'Please upload an image first.';
+      });
       return;
     }
 
     final request = http.MultipartRequest(
       'POST',
-      Uri.parse('http://127.0.0.1:5000/detect_image'), 
+      Uri.parse('http://10.0.2.2:5000/detect_image'),
     );
     request.files.add(await http.MultipartFile.fromPath('image', _image!.path));
 
-    final response = await request.send();
-    if (response.statusCode == 200) {
-      final responseData = await response.stream.toBytes();
-      final result = json.decode(String.fromCharCodes(responseData));
-      print(result);
-    } else {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error detecting image.')),
-      );
+    try {
+      final response = await request.send();
+      if (response.statusCode == 200) {
+        final responseData = await response.stream.bytesToString();
+        final result = json.decode(responseData);
+
+        setState(() {
+          _result = result.toString();
+          _errorMessage = '';
+        });
+      } else {
+        setState(() {
+          _errorMessage =
+              'Error detecting image. Status code: ${response.statusCode}'; // Set error message
+          _result = '';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _errorMessage = 'An error occurred: $e';
+        _result = '';
+      });
     }
   }
+
   @override
   Widget build(BuildContext context) {
-    return   Scaffold(
-        body:Stack(
-          children: [
-               Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, children: [
-            ProfileBar(),
-            Spacer(),
-            ElevatedButton(onPressed: _uploadImage, child: Text('Upload')),
-            SizedBox(height: 10,),
-            Main_Button(text: 'Detect', icon: Icon(Icons.error), route: '/detecting', onTap: (){_detectImage(context);}),
-            Spacer(),
-            Spacer(),
-          ]),
-        
+    return Scaffold(
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ProfileBar(),
+                Spacer(),
+                if (_image != null) Image.file(_image!),
+                ElevatedButton(
+                  onPressed: _uploadImage,
+                  child: Text('Upload'),
+                ),
+                SizedBox(height: 10),
+                Main_Button(
+                  text: 'Detect',
+                  icon: Icon(Icons.error),
+                  route: '/detecting',
+                  onTap: () {
+                    _detectImage(context);
+                  },
+                ),
+                SizedBox(height: 20),
+                if (_errorMessage.isNotEmpty)
+                  Text(_errorMessage, style: TextStyle(color: Colors.red)),
+                if (_result.isNotEmpty) Text('Result: $_result'),
+                Spacer(),
+              ],
+            ),
+          ),
+          AssistiveBall(),
+        ],
       ),
-      AssistiveBall()
-      ]
-    ));
+    );
   }
 }
-
