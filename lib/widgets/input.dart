6ca909a1fe5352bbf827ee99d85aaf45b80c733f
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-04 23:42:42 +0200

    Fixing Input

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -1,34 +1,47 @@
-
 import 'package:flutter/material.dart';
+
 class Input extends StatelessWidget {
-   final String text;
-   final Icon icon;
-   final double height;
-   final int maxLines;
-   final TextEditingController controller;
+  final String text;
+  final Icon icon;
+  final double height;
+  final int maxLines;
+  final TextEditingController controller;
 
-
-   const Input({super.key, required this.text, required this.icon, required this.height, required this.maxLines, required this.controller});
+  const Input(
+      {super.key,
+      required this.text,
+      required this.icon,
+      required this.height,
+      required this.maxLines,
+      required this.controller});
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 270,
-      height: height,
-      padding: EdgeInsets.fromLTRB(20,0,20,0),
-      
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        color:Theme.of(context).secondaryHeaderColor,
-      ),
-      child: TextFormField(
+        width: 270,
+        height: height,
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+              color: Theme.of(context).primaryColor,
+              spreadRadius: 0,
+              blurRadius: 1,
+              offset: const Offset(0, 4),
+            ),
+          ],
+          borderRadius: BorderRadius.circular(10),
+          color: Theme.of(context).secondaryHeaderColor,
+        ),
+        child: TextFormField(
           controller: controller,
           maxLines: maxLines,
           decoration: InputDecoration(
-          filled: true,
-          fillColor: Theme.of(context).secondaryHeaderColor,
-          icon: icon,
-          labelText: text,
-        ),
-    ));
+            border: InputBorder.none,
+            filled: true,
+            fillColor: Theme.of(context).secondaryHeaderColor,
+            icon: icon,
+            labelText: text,
+          ),
+        ));
   }
 }
