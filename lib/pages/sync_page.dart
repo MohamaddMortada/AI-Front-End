b10acd850218ab738c702ed632483158c3ad034e
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-08 18:02:38 +0200

    Creating generateKey

--- lib/pages/sync_page.dart
+++ lib/pages/sync_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
 class SyncPage extends StatefulWidget {
@@ -5,6 +7,22 @@ class SyncPage extends StatefulWidget {
   _SyncPageState createState() => _SyncPageState();
 }
 class _SyncPageState extends State<SyncPage> {
+
+  String syncKey = "";
+
+  Future<void> generateKey() async {
+    final response = await http.post(
+      Uri.parse('http://127.0.0.1:8000/api/generate-key'),
+      body: {'user_id': '2'},
+    );
+
+    if (response.statusCode == 201) {
+      final data = json.decode(response.body);
+      setState(() {
+        syncKey = data['sync_key'];
+      });
+    }
+  }
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
