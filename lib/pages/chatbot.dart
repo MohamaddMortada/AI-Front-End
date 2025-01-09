f30a68880237f2f933d31b196d0bb7271e4089bd
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-09 18:52:55 +0200

    Creating sendMessage

--- lib/pages/chatbot.dart
+++ lib/pages/chatbot.dart
@@ -1,7 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:front_end/widgets/input.dart';
 import 'package:front_end/widgets/profile_widget.dart';
+import 'package:http/http.dart' as http;
+
 
 class Chatbot extends StatefulWidget {
   @override
@@ -11,7 +15,27 @@ class Chatbot extends StatefulWidget {
 class _ChatbotState extends State<Chatbot> {  
     final TextEditingController messageController = TextEditingController();
      List<Map<String, String>> messages = [];
-     
+
+     Future<void> sendMessage(String message) async {
+    final url = Uri.parse('http://10.0.2.2:8000/api/chatbot'); 
+    final response = await http.post(
+      url,
+      body: {'message': message},
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      setState(() {
+        messages.add({"role": "user", "content": message});
+        messages.add({"role": "bot", "content": data['response']});
+      });
+    } else {
+      setState(() {
+        messages.add({"role": "bot", "content": "Failed to get response"});
+      });
+    }
+  }
+
 
 @override
   Widget build(BuildContext context) {
