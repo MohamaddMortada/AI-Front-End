52884d2441af1af9cd7a102bce3e4c49bc7dae93
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-09 20:23:04 +0200

    Adding bot profile

--- lib/pages/chatbot.dart
+++ lib/pages/chatbot.dart
@@ -6,18 +6,17 @@ import 'package:front_end/widgets/input.dart';
 import 'package:front_end/widgets/profile_widget.dart';
 import 'package:http/http.dart' as http;
 
-
 class Chatbot extends StatefulWidget {
   @override
   _ChatbotState createState() => _ChatbotState();
 }
 
-class _ChatbotState extends State<Chatbot> {  
-    final TextEditingController messageController = TextEditingController();
-     List<Map<String, String>> messages = [];
+class _ChatbotState extends State<Chatbot> {
+  final TextEditingController messageController = TextEditingController();
+  List<Map<String, String>> messages = [];
 
-     Future<void> sendMessage(String message) async {
-    final url = Uri.parse('http://10.0.2.2:8000/api/chatbot'); 
+  Future<void> sendMessage(String message) async {
+    final url = Uri.parse('http://10.0.2.2:8000/api/chatbot');
     final response = await http.post(
       url,
       body: {'message': message},
@@ -36,59 +35,85 @@ class _ChatbotState extends State<Chatbot> {
     }
   }
 
-
-@override
+  @override
   Widget build(BuildContext context) {
-    return    Scaffold(
-        body: 
-          
-Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                final message = messages[index];
-                return Align(
-                  alignment: message['role'] == 'user'
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  child: Container(
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.all(5),
-                    decoration: BoxDecoration(
-                      color:Color.fromARGB(255, 201, 218, 223),
-                      borderRadius: BorderRadius.circular(8),
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 139, 166, 177), 
+              Color.fromARGB(255, 19, 99, 134), 
+              Color.fromARGB(255, 1, 71, 98), 
+              Color.fromARGB(255, 0, 41, 57), 
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Column(
+          children: [
+             Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child:CircleAvatar(
+    radius: 80,
+    backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+    backgroundImage: const AssetImage('assets/Chatbot-girl.webp'),
+             )),
+            Expanded(
+              child: ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  final message = messages[index];
+                  return Align(
+                    alignment: message['role'] == 'user'
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      margin: const EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 225, 245, 255),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Text(
+                        message['content'] ?? '',
+                        style: const TextStyle(color: Colors.black),
+                      ),
                     ),
-                    child: Text(message['content'] ?? ''),
-                  ),
-                );
-              },
-            ),),
-            Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Input(
-              
-              text: 'Type your message here!', image: Image.asset('assets/Icons/type.png'), height: 45, maxLines: 1, controller: messageController,
+                  );
+                },
               ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send,color: Theme.of(context).primaryColor,),
-                  onPressed: () {
-                    final message = messageController.text;
-                    if (message.isNotEmpty) {
-                      sendMessage(message);
-                      messageController.clear();
-                    }
-                  },
-                ),
-            
-            ]))]));
-      }
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Input(
+                      text: 'Type your message here!',
+                      image: Image.asset('assets/Icons/type.png'),
+                      height: 45,
+                      maxLines: 1,
+                      controller: messageController,
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(
+                      size: 30,
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                    onPressed: () {
+                      final message = messageController.text;
+                      if (message.isNotEmpty) {
+                        sendMessage(message);
+                        messageController.clear();
+                      }
+                    },
+                  ),
+                ],
+              ),
+            ),],),),);
+        }
 }
-
-  
-    
