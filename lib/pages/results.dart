bae8075c79e9ec7c60a112492ab987927338e126
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-10 16:34:36 +0200

    Creating Results Page

---
+++ lib/pages/results.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Results extends StatefulWidget {
+  @override
+  _ResultsState createState() => _ResultsState();
+}
+
+class _ResultsState extends State<Results> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
