5f3ff15352fce6d9556e43fa58bfcfe8e134651a
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-13 22:09:23 +0200

    Fixing Designs

--- lib/widgets/image_button.dart
+++ lib/widgets/image_button.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:front_end/widgets/button_secondary.dart';
-import 'package:front_end/widgets/text_field.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 class ImageButton extends StatelessWidget {
   final String imagePath;
@@ -10,66 +7,91 @@ class ImageButton extends StatelessWidget {
   final String discription;
   final String route;
 
-  const ImageButton(
-      {super.key,
-      required this.imagePath,
-      required this.text,
-      required this.discription,
-      required this.route});
+  const ImageButton({
+    super.key,
+    required this.imagePath,
+    required this.text,
+    required this.discription,
+    required this.route,
+  });
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-          Get.toNamed(route);
-        },
-        child: Container(
-          decoration: BoxDecoration(
-              color: Color.fromARGB(255, 128, 168, 183),
-              borderRadius: BorderRadius.circular(10)),
-          width: 330,
-          height: 150,
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 6),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Column(
+      onTap: () {
+        Get.toNamed(route);
+      },
+      child: Container(
+        width: 330,
+        height: 150,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 128, 168, 183),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Flexible(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Title',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+                    text,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
                   ),
                   Text(
                     discription,
-                    style: TextStyle(fontWeight: FontWeight.w500, fontSize: 14),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      color: Colors.black54,
+                    ),
                   ),
                   GestureDetector(
-                      onTap: () {
-                        Get.toNamed(route);
-                      },
-                      child: Container(
-                        child: Text(
-                          text,
-                          style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold),
+                    onTap: () {
+                      Get.toNamed(route);
+                    },
+                    child: Container(
+                      alignment: Alignment.center,
+                      width: 100,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: const Text(
+                        'Learn More',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
                         ),
-                        width: 100,
-                        height: 40,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      ))
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              Container(
-                  child: Image.asset(
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Image.asset(
                 imagePath,
-                width: 50,
-                height: 50,
-              )),
-            ],
-          ),
-        ));
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
