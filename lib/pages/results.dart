eac6f5b62ac7bd75e3960fcc980b182dce12a6e8
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-14 01:29:43 +0200

    Adding Image to Results Page

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -10,9 +10,9 @@ class Results extends StatefulWidget {
 }
 
 class _ResultsState extends State<Results> {
-    final TextEditingController _trainingController = TextEditingController();
+  final TextEditingController _trainingController = TextEditingController();
 
-Future<void> sendResult() async {
+  Future<void> sendResult() async {
     final session = _trainingController.text.trim();
     if (session.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -21,15 +21,12 @@ Future<void> sendResult() async {
       return;
     }
 
-    final url = Uri.parse('http://10.0.2.2:8000/api/results'); 
-  try {
+    final url = Uri.parse('http://10.0.2.2:8000/api/results');
+    try {
       final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
-        body: json.encode({
-          'result': session,
-          'user_id':2
-          }),
+        body: json.encode({'result': session, 'user_id': 2}),
       );
 
       if (response.statusCode == 200) {
@@ -44,19 +41,50 @@ Future<void> sendResult() async {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Error: ${error.toString()}")),
       );
-    } 
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Input(text: 'Enter a training session', image: Image.asset('assets/Icons/type.png'), height: 270, maxLines: 10, controller: _trainingController),
-              SizedBox(height: 10,),
-              ButtonSecondary(text: 'Add Result', image: Image.asset('assets/Icons/add.png'), onTap: ()async{sendResult();})
-  
-            ])));
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      const Spacer(),
+      const Spacer(),
+
+      const Text(
+        'Sessions Section',
+        style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+        textAlign: TextAlign.center,
+      ),
+      const Spacer(),
+      Image.asset(
+        'assets/results.png',
+        width: 200,
+        height: 200,
+      ),
+      const Spacer(),
+
+      Input(
+          text: 'Enter a training session/result',
+          image: Image.asset('assets/Icons/type.png'),
+          height: 270,
+          maxLines: 10,
+          controller: _trainingController),
+      const SizedBox(
+        height: 10,
+      ),
+      ButtonSecondary(
+          text: 'Add Result',
+          image: Image.asset('assets/Icons/add.png'),
+          onTap: () async {
+            sendResult();
+          }),
+      const Spacer(),
+      const Spacer(),
+      const Spacer(),
+      const Spacer(),
+    ])));
   }
 }
