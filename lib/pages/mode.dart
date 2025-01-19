b9b201e91d1d1050ddf5259815c1b7459ff08f8d
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-19 12:00:40 +0200

    Adding back button to ModePage

--- lib/pages/mode.dart
+++ lib/pages/mode.dart
@@ -1,27 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:front_end/widgets/button.dart';
-import 'package:front_end/widgets/button_secondary.dart';
 import 'package:get/get.dart';
+import 'package:front_end/widgets/profile_bar.dart';
 
 class ModePage extends StatelessWidget {
   @override
-
   Widget build(BuildContext context) {
     return Scaffold(
-     
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/chronometer.png', width: 300,height: 300,),
-            SizedBox(height: 20),
-            Main_Button(text: 'Start Line', image: Image.asset('assets/Icons/add.png'), onTap: (){Get.toNamed('/validate');}, route: '/validate',),
-            SizedBox(height: 20),
-            Main_Button(text: 'Finish Line', image: Image.asset('assets/Icons/add.png'), onTap: (){Get.toNamed('/sync');},route:'/sync'),
-            SizedBox(height: 20),
-            
-          ],
-        ),
+      body: Stack(
+        children: [
+          Column(
+            children: [
+              Stack(
+                children: [
+                  const ProfileBar(),
+                  Positioned(
+                    left: 10,
+                    top: 20, 
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.of(context).pop(); 
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Center(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/chronometer.png',
+                      width: 200,
+                      height: 200,
+                    ),
+                    const SizedBox(height: 40),
+                    Main_Button(
+                      text: 'Start Line',
+                      image: Image.asset('assets/Icons/add.png'),
+                      onTap: () {
+                        Get.toNamed('/validate');
+                      },
+                      route: '/validate',
+                    ),
+                    const SizedBox(height: 20),
+                    Main_Button(
+                      text: 'Finish Line',
+                      image: Image.asset('assets/Icons/add.png'),
+                      onTap: () {
+                        Get.toNamed('/sync');
+                      },
+                      route: '/sync',
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(flex: 3), 
+            ],
+          ),
+        ],
       ),
     );
   }
