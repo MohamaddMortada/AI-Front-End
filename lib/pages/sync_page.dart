ba76f11375682fce676cc7aa13865730ec387f59
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-19 12:22:47 +0200

    Updating changes for SyncPage

--- lib/pages/sync_page.dart
+++ lib/pages/sync_page.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:front_end/widgets/button_secondary.dart';
+import 'package:front_end/widgets/profile_bar.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:front_end/widgets/button_secondary.dart';
 
 class SyncPage extends StatefulWidget {
   @override
@@ -11,14 +12,7 @@ class SyncPage extends StatefulWidget {
 }
 
 class _SyncPageState extends State<SyncPage> {
-
   String syncKey = "";
-  int? timeDifference;
-
-  Future<void> _saveSyncKey(String key) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setString('sync_key', key);  
-  }
 
   Future<void> generateKey() async {
     final response = await http.post(
@@ -32,14 +26,15 @@ class _SyncPageState extends State<SyncPage> {
         syncKey = data['sync_key'];
       });
 
-      await _saveSyncKey(syncKey);
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setString('sync_key', syncKey);
 
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Sync Key generated and saved!'))
+        const SnackBar(content: Text('Sync Key generated and saved!')),
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to generate Sync Key'))
+        const SnackBar(content: Text('Failed to generate Sync Key')),
       );
     }
   }
@@ -47,37 +42,63 @@ class _SyncPageState extends State<SyncPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+      body: Stack(
+        children: [
+          Column(
             children: [
-              Text("Sync Key: $syncKey", style: const TextStyle(fontSize: 18)),
-              if (timeDifference != null)
-                Text("Time Difference: $timeDifference seconds", style: const TextStyle(fontSize: 18)),
-
-              ButtonSecondary(
-                text: "Generate Key",
-                image: Image.asset('assets/Icons/add.png'),
-                onTap: () async {
-                  await generateKey();
-                },
+              Stack(
+                children: [
+                  const ProfileBar(),
+                  Positioned(
+                    left: 10,
+                    top: 20, 
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.of(context).pop(); 
+                      },
+                    ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 10),
-              if(syncKey != '')
-              ButtonSecondary(
-                text: "Get to finish line",
-                image: Image.asset('assets/Icons/add.png'),
-                onTap: () {
-                  Get.toNamed('/analyze');
-                },
+              const SizedBox(height: 50),
+              const Text(
+                'Key Generator',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 20),
+              Image.asset(
+                'assets/synckey.png',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(height: 40),
+              if (syncKey.isNotEmpty)
+                Text(
+                  "Sync Key: $syncKey",
+                  style: const TextStyle(fontSize: 16),
+                ),
+              const SizedBox(height: 20),
+              if (syncKey.isNotEmpty)
+                Container(
+                  color: const Color.fromARGB(255, 110, 168, 187),
+                  child: QrImageView(
+                    data: syncKey,
+                    version: QrVersions.auto,
+                    size: 200.0,
+                  ),
+                ),
+              const SizedBox(height: 20),
+              ButtonSecondary(
+                  text: "Generate Key",
+                  image: Image.asset('assets/Icons/white-key.png'),
+                  onTap: () async {
+                    await generateKey();
+                  }),
+              const Spacer(flex: 2),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
