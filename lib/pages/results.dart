1e83b7eef68cec125148ff70d0d04d84495abfd9
mohamaddmortada <mortada_mohamad@hotmail.com>
2025-01-19 12:34:46 +0200

    Adding back button to Results Page

--- lib/pages/results.dart
+++ lib/pages/results.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:front_end/widgets/button_secondary.dart';
 import 'package:front_end/widgets/input.dart';
+import 'package:front_end/widgets/profile_bar.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
@@ -17,14 +18,11 @@ class _ResultsState extends State<Results> {
   @override
   void initState() {
     super.initState();
-
-     fetchResults();
+    fetchResults();
   }  
 
-
-
   Future<void> fetchResults() async {
-    final url = Uri.parse('http://192.168.44.188:8000/api/getresult');
+    final url = Uri.parse('http://192.168.199.124:8000/api/getresult');
     try {
       final response = await http.post(
         url,
@@ -58,7 +56,7 @@ class _ResultsState extends State<Results> {
       return;
     }
 
-    final url = Uri.parse('http://192.168.44.188:8000/api/results');
+    final url = Uri.parse('http://192.168.199.124:8000/api/results');
     try {
       final response = await http.post(
         url,
@@ -87,62 +85,81 @@ class _ResultsState extends State<Results> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          const SizedBox(height: 40), 
-          const Text(
-            'Sessions Section',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: 20),
-          Image.asset(
-            'assets/results.png',
-            width: 200,
-            height: 200,
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Input(
-              text: 'Enter a training session/result',
-              image: Image.asset('assets/Icons/type.png'),
-              height: 150,
-              maxLines: 6,
-              controller: _trainingController,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ButtonSecondary(
-              text: 'Add Result',
-              image: Image.asset('assets/Icons/add.png'),
-              onTap: sendResult,
-            ),
-          ),
-          const SizedBox(height: 20),
-          Expanded(
-            flex: 2, 
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: _results.isEmpty
-                  ? const Center(
-                      child: Text("No results available."),
-                    )
-                  : ListView.builder(
-                      itemCount: _results.length,
-                      itemBuilder: (context, index) {
-                        final result = _results[index];
-                        return Card(
-                          margin: const EdgeInsets.symmetric(vertical: 8),
-                          child: ListTile(
-                            title: Text('Session: ${result['result']}'),
-                          ),
-                        );
+          Column(
+            children: [
+              Stack(
+                children: [
+                  const ProfileBar(),
+                  Positioned(
+                    left: 10,
+                    top: 20,
+                    child: IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      onPressed: () {
+                        Navigator.of(context).pop(); 
                       },
                     ),
-            ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'SESSIONS SECTION',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 20),
+              Image.asset(
+                'assets/results.png',
+                width: 200,
+                height: 200,
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Input(
+                  text: 'Enter a training session/result',
+                  image: Image.asset('assets/Icons/type.png'),
+                  height: 120,
+                  maxLines: 6,
+                  controller: _trainingController,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ButtonSecondary(
+                  text: 'Add Result',
+                  image: Image.asset('assets/Icons/add.png'),
+                  onTap: sendResult,
+                ),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                flex: 2, 
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: _results.isEmpty
+                      ? const Center(
+                          child: Text("No results available."),
+                        )
+                      : ListView.builder(
+                          itemCount: _results.length,
+                          itemBuilder: (context, index) {
+                            final result = _results[index];
+                            return Card(
+                              margin: const EdgeInsets.symmetric(vertical: 8),
+                              child: ListTile(
+                                title: Text('Session: ${result['result']}'),
+                              ),
+                            );
+                          },
+                        ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
